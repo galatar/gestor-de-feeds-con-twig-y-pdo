8f2559fa3a274e5f5af76e9a84fb9adc78d759bd
Juanan Ruiz <juanan@us.es>
2018-08-14 20:02:36 +0200

    Carga los feeds desde la base de datos

--- src/inicio.php
+++ src/inicio.php
@@ -3,27 +3,30 @@ global $plantilla;
 global $datos;
 global $dbh;
 
-$feeds = array(
-    "https://gonzalo123.com/feed/",
-    "https://www.htmlgoodies.com/rss.xml",
-);
-$reserva = array(
-    "https://news.ycombinator.com/rss",
-    "https://www.tomasvotruba.cz/rss",
-    "http://galatar.com/feed/",
-    "https://www.killerphp.com/articles/feed/",
-    "https://www.hitsubscribe.com/blog/feed/",
-    "https://jesuslc.com/blog/feed/",
-    "http://feeds.feedburner.com/desarrolloweb/novedades-articulos",
-    "https://blog.phpdeveloper.org/feed/",
-    "https://frontendfoc.us/rss/",
-    "https://www.dreamhost.com/blog/feed/",
-    "http://ellatidodelucifer.blogspot.com/atom.xml",
-);
+//$feeds = array(
+//    "https://gonzalo123.com/feed/",
+//    "https://www.htmlgoodies.com/rss.xml",
+//);
+//$reserva = array(
+//    "https://news.ycombinator.com/rss",
+//    "https://www.tomasvotruba.cz/rss",
+//    "http://galatar.com/feed/",
+//    "https://www.killerphp.com/articles/feed/",
+//    "https://www.hitsubscribe.com/blog/feed/",
+//    "https://jesuslc.com/blog/feed/",
+//    "http://feeds.feedburner.com/desarrolloweb/novedades-articulos",
+//    "https://blog.phpdeveloper.org/feed/",
+//    "https://frontendfoc.us/rss/",
+//    "https://www.dreamhost.com/blog/feed/",
+//    "http://ellatidodelucifer.blogspot.com/atom.xml",
+//);
 
+$query = $dbh->prepare('SELECT * FROM feed WHERE favorito = 1;');
+$query->setFetchMode(PDO::FETCH_ASSOC);
+$query->execute();
 $entradas = array();
-foreach ($feeds as $feed) {
-    $xml = simplexml_load_file($feed);
+while ($feed = $query->fetch()) {
+    $xml = simplexml_load_file($feed['url']);
     if (is_object($xml)) {
         $entradas = array_merge($entradas, $xml->xpath("//item"));
     }
