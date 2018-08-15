754857999016a7b43b254416cede543b44c27699
Juanan Ruiz <juanan@us.es>
2018-08-15 19:48:56 +0200

    Cambia agregar_feed por mostrar_feed para hacerlo más versátil Cambia la variable entrada por articulo por estar más en contexto Limpia basurilla y pruebas

--- src/inicio.php
+++ src/inicio.php
@@ -5,31 +5,20 @@ $feeds = array(
     "https://www.tomasvotruba.cz/rss",
     "http://galatar.com/feed/",
     "https://www.killerphp.com/articles/feed/",
-    "https://www.hitsubscribe.com/blog/feed/",
-    "https://news.ycombinator.com/rss",
-    "https://jesuslc.com/blog/feed/",
-    "http://feeds.feedburner.com/desarrolloweb/novedades-articulos",
-    "https://blog.phpdeveloper.org/feed/",
-    "https://frontendfoc.us/rss/",
-    "https://www.dreamhost.com/blog/feed/",
-);
-$reserva = array(
-    "http://ellatidodelucifer.blogspot.com/atom.xml",
 );
 
-$entradas = array();
+$articulos = array();
 foreach ($feeds as $feed) {
     $xml = simplexml_load_file($feed);
-    $entradas = array_merge($entradas, $xml->xpath("//item"));
-//    $entradas = array_merge($entradas, $xml->xpath("//item/pubdate[text() = '2018']"));
+    $articulos = array_merge($articulos, $xml->xpath("//item"));
 }
-//Ordena las entradas por fecha de publicación
-usort($entradas, function ($feed1, $feed2) {
+//Ordena las articulos por fecha de publicación
+usort($articulos, function ($feed1, $feed2) {
     return strtotime($feed2->pubDate) - strtotime($feed1->pubDate);
 });
 
 try {
-    $twig->display('inicio.html.twig', array('entradas' => $entradas));
+    $twig->display('inicio.html.twig', array('articulos' => $articulos));
 } catch (Twig_Error_Loader $e) {
     var_dump($e);
 } catch (Twig_Error_Runtime $e) {
--- src/mostrar_feed.php
+++ src/mostrar_feed.php
@@ -6,13 +6,13 @@ if (filter_has_var(INPUT_POST, 'feed')) {
     $feed = filter_input(INPUT_POST, 'feed', FILTER_SANITIZE_STRING);
     $variables['feed'] = $feed;
     $xml = simplexml_load_file($feed);
-    if ($entradas = $xml->xpath("//item")) {
-        $variables['entradas'] = $entradas;
+    if ($articulos = $xml->xpath("//item")) {
+        $variables['articulos'] = $articulos;
     }
 }
 
 try {
-    $twig->display('agregar_feed.html.twig', $variables);
+    $twig->display('mostrar_feed.html.twig', $variables);
 } catch (Twig_Error_Loader $e) {
     var_dump($e);
 } catch (Twig_Error_Runtime $e) {
